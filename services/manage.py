53a66a84e57d81634b6a7b1e52959f63e9de8b70
chreman <web@christopherkittel.eu>
2018-12-04 20:23:12 +0100

    fixed db recreation

--- services/manage.py
+++ services/manage.py
@@ -1,8 +1,9 @@
 from cassandra.cluster import Cluster
 from cassandra.cqlengine import connection
-from cassandra.cqlengine.management import sync_table
+from cassandra.cqlengine.management import create_keyspace_simple, drop_keyspace, drop_table, sync_table
 
 from flask.cli import FlaskGroup
+from databases import db
 from app import new_ifs_app
 from models import Documents
 
@@ -14,16 +15,10 @@ cli = FlaskGroup(create_app=new_ifs_app)
 def recreate_db():
     """Cleans everything (!) and sets up database"""
 
-    cluster = Cluster(['192.168.1.1', '192.168.1.2', '127.0.0.1'])
-    session = cluster.connect()
-    connection.set_session(session)
-
-    session.execute('DROP KEYSPACE IF EXISTS documents;')
-    session.execute("""CREATE KEYSPACE IF NOT EXISTS documents
-                       WITH REPLICATION = {
-                        'class' : 'SimpleStrategy', 'replication_factor' : 1
-                        }
-                    """)
-    session.set_keyspace('documents')
-    session.execute('DROP TABLE IF EXISTS documents;')
+    drop_keyspace('documents')
+    drop_table(Documents)
+    create_keyspace_simple('documents', replication_factor=1)
     sync_table(Documents)
+
+if __name__ == '__main__':
+    cli()
